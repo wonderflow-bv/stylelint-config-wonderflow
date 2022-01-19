685bcaab9a8872d4811bc5ae1297e5826a673a81
Mattia <astorino.mattia@gmail.com>
2022-01-19 13:37:33 +0100

    fix: remove hardcoded path

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 module.exports = {
-  extends: 'stylelint-config-equinusocio',
-  plugins: ['stylelint-design-tokens-plugin'],
-  rules: {
-    'designtokens/check': [
-      true,
-      './node_modules/@wonderflow/tokens/platforms/web/tokens.json'
-    ]
-  }
+  extends: 'stylelint-config-equinusocio'
 }
